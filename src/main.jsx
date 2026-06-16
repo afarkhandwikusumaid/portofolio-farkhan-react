4095d07d6f14bc348e03a4f110a2159764b0a728
A. Farkhan Dwi Kusuma <afarkhandwikusuma.id@gmail.com>
2026-06-16 17:47:28 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <HelmetProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
